f5d428c701566b2c5911e9ecd22019a80a5e8bcb
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2023-10-05 18:39:45 -0500

    puntos 8,9,10 y 11 Front-end-logica proxima semana

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -10,7 +10,7 @@ apimock=(function(){
 	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
 
 	 mockdata["Luisa"]=[{author:"Luisa","points":[{"x":150,"y":180},{"x":115,"y":120}],"name":"Plano1"},
-	 {author:"Luisa","points":[{"x":150,"y":180},{"x":115,"y":120}],"name":"Plano2"}];
+	 {author:"Luisa","points":[{"x":250,"y":80},{"x":115,"y":-120}],"name":"Plano2"}];
 
 	 mockdata["Karol"]=[{author:"Karol","points":[{"x":150,"y":150},{"x":150,"y":-150}],"name":"CasaAzul"},
 	 {author:"Karol","points":[{"x":110,"y":0},{"x":150,"y":130},{"x":150,"y":0},{"x":110,"y":130}],"name":"CasaVerde"}]; 
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -19,8 +19,8 @@ app = (function(){
         _blueprints.map((blueprint) => {
             $("#result-blueprints-detail").append(
                 "<tr><td>" + blueprint.name + "</td>" +
-                "<td>" + blueprint.puntos + "</td>" 
-                //"<td><input id=" + blueprint.name + " type = 'button' onclick='app.printBlueprint(this)' value='Open'/></td></tr>"                 
+                "<td>" + blueprint.puntos + "</td>" +
+                "<td><input id=" + blueprint.name + " type = 'button' onclick='app.printBlueprint(this)' value='Open'/></td></tr>"                 
             );
         });
 
@@ -39,10 +39,34 @@ app = (function(){
         
     };
 
+    var _printCanvas = function(blueprintModel) {
+        $("#actual-name").text("Current blueprint: " + blueprintModel.name);
+        const puntos = blueprintModel.points;
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.restore();
+        ctx.beginPath();
+        for (let i = 1; i < puntos.length; i++) {
+            ctx.moveTo(puntos[i - 1].x, puntos[i - 1].y);
+            ctx.lineTo(puntos[i].x, puntos[i].y);
+            if (i === puntos.length - 1) {
+                ctx.moveTo(puntos[i].x, puntos[i].y);
+                ctx.lineTo(puntos[0].x, puntos[0].y);
+            }
+        }
+        ctx.stroke();
+    };
+
     var innerMockModule = {
         getAuthorBlueprints: function () {
             let author = $("#author-name").val();
             actualizarBlueprint(author);
+        },
+
+        printBlueprint: function(blueprintDOM){
+            let author = $("#author-name").val();
+            apimock.getBlueprintsByNameAndAuthor(author, blueprintDOM.id, _printCanvas);
         }
     };
     return innerMockModule;
